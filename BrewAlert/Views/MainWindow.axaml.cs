2e72e2b309e9826adba5778ce565070a77eca737
dogukanozen <dogukannozen@hotmail.com>
2026-04-22 12:36:45 +0300

    Add project files.

---
+++ BrewAlert/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BrewAlert.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
