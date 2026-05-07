1fe6d35dde032a6798ebac7674661021d8e57403
dogukanozen <dogukannozen@hotmail.com>
2026-05-07 09:06:37 +0300

    deploy test

--- src/BrewAlert.UI/App.axaml.cs
+++ src/BrewAlert.UI/App.axaml.cs
@@ -16,7 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-
+ 
 namespace BrewAlert.UI;
 
 public partial class App : Application
